078ad152f8af24d0381ed2e57e3eec233e5b8143
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-03-11 17:55:22 +0100

    completed a swap

--- contracts/UniswapV3CrossFlash.sol
+++ contracts/UniswapV3CrossFlash.sol
@@ -2,31 +2,43 @@
 pragma solidity >=0.7.6;
 pragma abicoder v2;
 
-import '@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol';
-import '@uniswap/v3-core/contracts/libraries/LowGasSafeMath.sol';
+import "@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol";
+import "@uniswap/v3-core/contracts/libraries/LowGasSafeMath.sol";
 
-import '@uniswap/v3-periphery/contracts/base/PeripheryPayments.sol';
-import '@uniswap/v3-periphery/contracts/base/PeripheryImmutableState.sol';
-import '@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol';
-import '@uniswap/v3-periphery/contracts/libraries/CallbackValidation.sol';
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import "@uniswap/v3-periphery/contracts/base/PeripheryPayments.sol";
+import "@uniswap/v3-periphery/contracts/base/PeripheryImmutableState.sol";
+import "@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol";
+import "@uniswap/v3-periphery/contracts/libraries/CallbackValidation.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "hardhat/console.sol";
+import "./interfaces/IUniswapV2Router01.sol";
+import "./interfaces/IUniswapV2Factory.sol";
 
 /// @title Flash contract implementation
 /// @notice An example contract using the Uniswap V3 flash function
-contract UniswapV3CrossFlash is IUniswapV3FlashCallback, PeripheryImmutableState, PeripheryPayments {
+contract UniswapV3CrossFlash is
+    IUniswapV3FlashCallback,
+    PeripheryImmutableState,
+    PeripheryPayments
+{
     using LowGasSafeMath for uint256;
     using LowGasSafeMath for int256;
 
     ISwapRouter public immutable swapRouter;
- 
+    address public immutable sushiSwapRouter;
+    address public sushiFactory = 0xc35DADB65012eC5796536bD9864eD8773aBc74C4;
+    uint256 constant MAX_INT =
+        115792089237316195423570985008687907853269984665640564039457584007913129639935;
+
     constructor(
         address _swapRouter,
         address _factory,
-        address _WETH9
+        address _WETH9,
+        address _sushiSwapRouterAddress
     ) PeripheryImmutableState(_factory, _WETH9) {
         swapRouter = ISwapRouter(_swapRouter);
+        sushiSwapRouter = _sushiSwapRouterAddress;
     }
 
     /// @param fee0 The fee from calling flash for token0
@@ -39,74 +51,98 @@ contract UniswapV3CrossFlash is IUniswapV3FlashCallback, PeripheryImmutableState
         uint256 fee1,
         bytes calldata data
     ) external override {
-        FlashCallbackData memory decoded = abi.decode(data, (FlashCallbackData));
+        FlashCallbackData memory decoded = abi.decode(
+            data,
+            (FlashCallbackData)
+        );
         CallbackValidation.verifyCallback(factory, decoded.poolKey);
 
-        address token0 = decoded.poolKey.token0;
-        address token1 = decoded.poolKey.token1;
+        uint256 borrowedAmount = decoded.amount0 > 0
+            ? decoded.amount0
+            : decoded.amount1;
 
+        address tokenBorrow = decoded.amount0 > 0
+            ? decoded.poolKey.token0
+            : decoded.poolKey.token1;
 
-        TransferHelper.safeApprove(token0, address(swapRouter), decoded.amount0);
-        TransferHelper.safeApprove(token1, address(swapRouter), decoded.amount1);
+        uint256 fee = decoded.amount0 > 0 ? fee0 : fee1;
+
+        TransferHelper.safeApprove(
+            tokenBorrow,
+            address(swapRouter),
+            borrowedAmount
+        );
+        TransferHelper.safeApprove(
+            tokenBorrow,
+            address(sushiFactory),
+            MAX_INT
+        );
+        TransferHelper.safeApprove(
+            decoded.arbToken2,
+            address(sushiFactory),
+            MAX_INT
+        );
 
         // profitable check
         // exactInputSingle will fail if this amount not met
-        uint256 amount1Min = LowGasSafeMath.add(decoded.amount1, fee1);
-        uint256 amount0Min = LowGasSafeMath.add(decoded.amount0, fee0);
-        console.log("Account Balance: token0 %s, token1: %s", IERC20(address(token0)).balanceOf(address(this)), IERC20(address(token1)).balanceOf(address(this)));
+        // uint256 amount1Min = LowGasSafeMath.add(decoded.amount1, fee1);
+        // uint256 amount0Min = LowGasSafeMath.add(decoded.amount0, fee0);
+        console.log(
+            "Account Balance: Arbtoken0 %s, arbToken1: %s",
+            IERC20(address(tokenBorrow)).balanceOf(address(this)),
+            IERC20(address(decoded.arbToken2)).balanceOf(address(this))
+        );
 
-        // call exactInputSingle for swapping token1 for token0 in pool w/fee2
-        uint256 amountOut0 =
-            swapRouter.exactInputSingle(
-                ISwapRouter.ExactInputSingleParams({
-                    tokenIn: token1,
-                    tokenOut: token0,
-                    fee: decoded.poolFee2,
-                    recipient: address(this),
-                    deadline: block.timestamp,
-                    amountIn: decoded.amount1,
-                    amountOutMinimum: amount0Min,
-                    sqrtPriceLimitX96: 0
-                })
-            );
+        require(
+            decoded.arbToken1 == tokenBorrow,
+            "arbToken1 should be the token borrowed"
+        );
+        console.log("borrow Amount 1 :%s", borrowedAmount);
+        // Perform arbitrage
+        uint256 trade1Amount = placeTradeUniswap(
+            decoded.arbToken1,
+            decoded.arbToken2,
+            borrowedAmount
+        );
 
-        // call exactInputSingle for swapping token0 for token 1 in pool w/fee3
-        uint256 amountOut1 =
-            swapRouter.exactInputSingle(
-                ISwapRouter.ExactInputSingleParams({
-                    tokenIn: token0,
-                    tokenOut: token1,
-                    fee: decoded.poolFee3,
-                    recipient: address(this),
-                    deadline: block.timestamp,
-                    amountIn: decoded.amount0,
-                    amountOutMinimum: amount1Min,
-                    sqrtPriceLimitX96: 0
-                })
-            );
+        console.log("trade Amount 1 :%s", trade1Amount);
+        uint256 trade2Amount = placeTradeSushi(
+            decoded.arbToken2,
+            decoded.arbToken1,
+            trade1Amount
+        );
 
         // end up with amountOut0 of token0 from first swap and amountOut1 of token1 from second swap
-        uint256 amount0Owed = LowGasSafeMath.add(decoded.amount0, fee0);
-        uint256 amount1Owed = LowGasSafeMath.add(decoded.amount1, fee1);
+        uint256 amountOwed = LowGasSafeMath.add(borrowedAmount, fee);
+        require(trade2Amount > amountOwed, "Trade not Profitable");
 
-        TransferHelper.safeApprove(token0, address(this), amount0Owed);
-        TransferHelper.safeApprove(token1, address(this), amount1Owed);
-
-        if (amount0Owed > 0) pay(token0, address(this), msg.sender, amount0Owed);
-        if (amount1Owed > 0) pay(token1, address(this), msg.sender, amount1Owed);
+        TransferHelper.safeApprove(tokenBorrow, address(this), amountOwed);
 
         // if profitable pay profits to payer
-        if (amountOut0 > amount0Owed) {
-            uint256 profit0 = LowGasSafeMath.sub(amountOut0, amount0Owed);
+        if (trade2Amount > amountOwed) {
+            uint256 profit = LowGasSafeMath.sub(trade2Amount, amountOwed);
 
-            TransferHelper.safeApprove(token0, address(this), profit0);
-            pay(token0, address(this), decoded.payer, profit0);
+            TransferHelper.safeApprove(tokenBorrow, address(this), profit);
+            pay(tokenBorrow, address(this), decoded.payer, profit);
         }
-        if (amountOut1 > amount1Owed) {
-            uint256 profit1 = LowGasSafeMath.sub(amountOut1, amount1Owed);
-            TransferHelper.safeApprove(token0, address(this), profit1);
-            pay(token1, address(this), decoded.payer, profit1);
-        }
+
+        // Payback loan
+        if (amountOwed > 0)
+            pay(tokenBorrow, address(this), msg.sender, amountOwed);
+
+        console.log(
+            "Account Balance After repay: tokenBorrow:  %s",
+            IERC20(address(tokenBorrow)).balanceOf(address(this))
+        );
+
+        console.log(
+            "Balance of Payer:  %s",
+            IERC20(address(tokenBorrow)).balanceOf(address(decoded.payer))
+        );
+    }
+
+    function getTokenBalance(address _token) public view returns (uint256) {
+        return IERC20(_token).balanceOf(address(this));
     }
 
     //fee1 is the fee of the pool from the initial borrow
@@ -115,6 +151,8 @@ contract UniswapV3CrossFlash is IUniswapV3FlashCallback, PeripheryImmutableState
     struct FlashParams {
         address token0;
         address token1;
+        address arbToken1;
+        address arbToken2;
         uint24 fee1;
         uint256 amount0;
         uint256 amount1;
@@ -125,21 +163,30 @@ contract UniswapV3CrossFlash is IUniswapV3FlashCallback, PeripheryImmutableState
     struct FlashCallbackData {
         uint256 amount0;
         uint256 amount1;
+        address arbToken1;
+        address arbToken2;
         address payer;
         PoolAddress.PoolKey poolKey;
         uint24 poolFee2;
         uint24 poolFee3;
-
     }
 
     /// @param params The parameters necessary for flash and the callback, passed in as FlashParams
     /// @notice Calls the pools flash function with data needed in `uniswapV3FlashCallback`
-    function initFlash(FlashParams memory params) external {
+    function initFlashUniswap(FlashParams memory params) external {
+        PoolAddress.PoolKey memory poolKey = PoolAddress.PoolKey({
+            token0: params.token0,
+            token1: params.token1,
+            fee: params.fee1
+        });
+        console.log(
+            " pool address: %s",
+            PoolAddress.computeAddress(factory, poolKey)
+        );
 
-        console.log(" this is token0: %s", params.token0, params.amount1);
-        PoolAddress.PoolKey memory poolKey =
-            PoolAddress.PoolKey({token0: params.token0, token1: params.token1, fee: params.fee1});
-        IUniswapV3Pool pool = IUniswapV3Pool(PoolAddress.computeAddress(factory, poolKey));
+        IUniswapV3Pool pool = IUniswapV3Pool(
+            PoolAddress.computeAddress(factory, poolKey)
+        );
         // recipient of borrowed amounts
         // amount of token0 requested to borrow
         // amount of token1 requested to borrow
@@ -153,13 +200,73 @@ contract UniswapV3CrossFlash is IUniswapV3FlashCallback, PeripheryImmutableState
                 FlashCallbackData({
                     amount0: params.amount0,
                     amount1: params.amount1,
+                    arbToken1: params.arbToken1,
+                    arbToken2: params.arbToken2,
                     payer: msg.sender,
                     poolKey: poolKey,
                     poolFee2: params.fee2,
                     poolFee3: params.fee3
-                  
                 })
             )
         );
+    }
+
+    function placeTradeUniswap(
+        address _fromToken,
+        address _toToken,
+        uint256 _amount
+    ) internal returns (uint256) {
+        // execute on uniswap
+        console.log("amount to trade %s ", _amount);
+        // call exactInputSingle for swapping token1 for token0 in pool w/fee2
+        uint256 amountOut = swapRouter.exactInputSingle(
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: _fromToken,
+                tokenOut: _toToken,
+                fee: 3000,
+                recipient: address(this),
+                deadline: block.timestamp + 2 days,
+                amountIn: _amount,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            })
+        );
+
+        console.log("Amount out from trade %s", amountOut);
+
+        return amountOut;
+    }
+
+    function placeTradeSushi(
+        address _fromToken,
+        address _toToken,
+        uint256 _amount
+    ) internal returns (uint256) {
+        // execute on sushiswap
+        address pair = IUniswapV2Factory(sushiFactory).getPair(
+            _fromToken,
+            _toToken
+        );
+        require(pair != address(0), "There is no Liquidity");
+        address[] memory path = new address[](2);
+        path[0] = _fromToken;
+        path[1] = _toToken;
+        uint256 amountsOutMin = IUniswapV2Router01(sushiSwapRouter)
+            .getAmountsOut(_amount, path)[1];
+        console.log("balance after first swap %s",IERC20(address(_fromToken)).balanceOf(address(this)));
+        console.log("minimum amounts out %s",amountsOutMin);
+
+        // call exactInputSingle for swapping token1 for token0 in pool w/fee2
+        uint256 amountOut = IUniswapV2Router01(sushiSwapRouter)
+            .swapExactTokensForTokens(
+                _amount,
+                amountsOutMin,
+                path,
+                address(this),
+                block.timestamp + 2 days
+            )[1];
+
+        return amountOut;
+        // execute on sushi
     }
 }
--- test/tester.js
+++ test/tester.js
@@ -17,15 +17,29 @@ describe("FlashSwap Contract", () => {
     _swapRouter = "0xE592427A0AEce92De3Edee1F18E0157C05861564",
     _factory = "0x1F98431c8aD98523631AE4a59f267346ea31F984",
     _WETH9 = "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+    _sushiSwapRouter = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
     txArbitrage;
 
-  const DECIMALS = 6;
+  const DECIMALS = 18;
+  const BUSD = "0x9C9e5fD8bbc25984B178FdCE6117Defa39d2db39";
 
+  // whales
+  const BUSD_WHALE = "0x5da11e3cad7d192dfc69ea84fe5c11071b2b66c5";
   const USDC_WHALE = "0x9810762578aCCF1F314320CCa5B72506aE7D7630";
+  const WETH_WHALE = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+  const WMATIC_WHALE = "0xba12222222228d8ba445958a75a0704d566bf2c8";
+
   const USDC = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+  const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
+  const WMATIC = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"
   const LINK = "0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39";
   const WETH = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
-  const BASE_TOKEN_ADDRESS = USDC;
+  const BASE_TOKEN_ADDRESS = WMATIC;
+
+  const priceToSqrtPrice = (price) => {
+    const result = BigInt(Math.floor(Math.sqrt(price) * 2 ** 96));
+    return result;
+  };
 
   const tokenBase = new ethers.Contract(BASE_TOKEN_ADDRESS, abi, provider);
 
@@ -34,13 +48,12 @@ describe("FlashSwap Contract", () => {
     const [owner] = await ethers.getSigners();
 
     // Ensure that the WHALE has a balance
-    const whale_balance = await provider.getBalance(USDC_WHALE);
-    console.log(whale_balance);
+    const whale_balance = await provider.getBalance(WMATIC_WHALE);
+    console.log(whale_balance.toString());
     // expect(whale_balance).not.equal("0");
 
     const amountToBorrowInHuman = "10000";
     BORROW_AMOUNT = ethers.utils.parseUnits(amountToBorrowInHuman, DECIMALS);
-    console.log(BORROW_AMOUNT.toString(), "HEYA");
     initialFundingHuman = "10000";
     FUND_AMOUNT = ethers.utils.parseUnits(initialFundingHuman, DECIMALS);
     const flashSwapFactory = await ethers.getContractFactory(
@@ -53,23 +66,27 @@ describe("FlashSwap Contract", () => {
       "UniswapV3CrossFlash"
     );
 
-    FLASHSWAPV3 = await flashSwapV3Swap.deploy(_swapRouter, _factory, _WETH9);
+    FLASHSWAPV3 = await flashSwapV3Swap.deploy(_swapRouter, _factory, _WETH9, _sushiSwapRouter);
     await FLASHSWAPV3.deployed();
+    console.log("balance of contract before funding : ", (await FLASHSWAPV3.getTokenBalance(WMATIC)).toString())
 
     await impersonateFundErc20(
       tokenBase,
-      USDC_WHALE,
+      WMATIC_WHALE,
       FLASHSWAP.address,
       initialFundingHuman,
       DECIMALS
     );
     await impersonateFundErc20(
       tokenBase,
-      USDC_WHALE,
+      WMATIC_WHALE,
       FLASHSWAPV3.address,
       initialFundingHuman,
       DECIMALS
     );
+
+    console.log("balance of contract after funding : ", (await FLASHSWAPV3.getTokenBalance(WMATIC)).toString())
+
   });
 
   describe("Uniswap V3 cross Exchange arbitrage", () => {
@@ -81,74 +98,76 @@ describe("FlashSwap Contract", () => {
 
     it("should call flash", async () => {
       const params = {
-        token0: USDC ,
-        token1: WETH,
+        token0: WMATIC ,
+        token1: USDC,
+        arbToken1: WMATIC,
+        arbToken2: DAI,
         fee1: 3000,
-        amount0: 0,
-        amount1: BORROW_AMOUNT,
+        amount0: BORROW_AMOUNT ,
+        amount1:  ethers.utils.parseUnits("0", 18),
         fee2: 3000,
         fee3: 3000,
       };
-      await FLASHSWAPV3.initFlash(params);
+      await FLASHSWAPV3.initFlashUniswap(params);
     });
   });
 
-  describe("Arbitrage Execution", () => {
-    it("ensures contract is funded", async () => {
-      const tokenBalance = await FLASHSWAP.getFlashContractBalance(
-        BASE_TOKEN_ADDRESS
-      );
+  // describe("Arbitrage Execution", () => {
+  //   it("ensures contract is funded", async () => {
+  //     const tokenBalance = await FLASHSWAP.getFlashContractBalance(
+  //       BASE_TOKEN_ADDRESS
+  //     );
 
-      const tokenBalances = await FLASHSWAP.getPairBalance();
+  //     const tokenBalances = await FLASHSWAP.getPairBalance();
 
-      const tokenBalanceInHuman = ethers.utils.formatUnits(
-        tokenBalance,
-        DECIMALS
-      );
-      expect(Number(tokenBalanceInHuman)).equal(Number(initialFundingHuman));
-    });
+  //     const tokenBalanceInHuman = ethers.utils.formatUnits(
+  //       tokenBalance,
+  //       DECIMALS
+  //     );
+  //     expect(Number(tokenBalanceInHuman)).equal(Number(initialFundingHuman));
+  //   });
 
-    it("it should check exchange to buy from ", async () => {
-      const buyExchange = await FLASHSWAP.checkSwapBuyLocation(
-        BORROW_AMOUNT,
-        USDC,
-        LINK
-      );
-      console.log(buyExchange);
-      expect(buyExchange).not.equal("");
-    });
+  //   it("it should check exchange to buy from ", async () => {
+  //     const buyExchange = await FLASHSWAP.checkSwapBuyLocation(
+  //       BORROW_AMOUNT,
+  //       USDC,
+  //       LINK
+  //     );
+  //     console.log(buyExchange);
+  //     expect(buyExchange).not.equal("");
+  //   });
 
-    it("excutes an arbitrage", async () => {
-      txArbitrage = await FLASHSWAP.startLoan(USDC, BORROW_AMOUNT);
+  //   it("excutes an arbitrage", async () => {
+  //     txArbitrage = await FLASHSWAP.startLoan(USDC, BORROW_AMOUNT);
 
-      const balanceAfterArbitrage = await FLASHSWAP.getFlashContractBalance(
-        BASE_TOKEN_ADDRESS
-      );
-      const formattedAmount = ethers.utils.formatUnits(
-        balanceAfterArbitrage,
-        DECIMALS
-      );
+  //     const balanceAfterArbitrage = await FLASHSWAP.getFlashContractBalance(
+  //       BASE_TOKEN_ADDRESS
+  //     );
+  //     const formattedAmount = ethers.utils.formatUnits(
+  //       balanceAfterArbitrage,
+  //       DECIMALS
+  //     );
 
-      const currentBalance = await FLASHSWAP.getFlashContractBalance(
-        BASE_TOKEN_ADDRESS
-      );
+  //     const currentBalance = await FLASHSWAP.getFlashContractBalance(
+  //       BASE_TOKEN_ADDRESS
+  //     );
 
-      const currentBalanceLINK = await FLASHSWAP.getFlashContractBalance(LINK);
+  //     const currentBalanceLINK = await FLASHSWAP.getFlashContractBalance(LINK);
 
-      assert(txArbitrage);
-    });
+  //     assert(txArbitrage);
+  //   });
 
-    it("provides GAS output", async () => {
-      const txReceipt = await provider.getTransactionReceipt(txArbitrage.hash);
-      const effGasPrice = txReceipt.effectiveGasPrice;
-      const txGasUsed = txReceipt.gasUsed;
-      const gasUsedEth = effGasPrice + txGasUsed;
+  //   it("provides GAS output", async () => {
+  //     const txReceipt = await provider.getTransactionReceipt(txArbitrage.hash);
+  //     const effGasPrice = txReceipt.effectiveGasPrice;
+  //     const txGasUsed = txReceipt.gasUsed;
+  //     const gasUsedEth = effGasPrice + txGasUsed;
 
-      console.log(
-        "Total Gas USED: " +
-          ethers.utils.formatEther(gasUsedEth.toString()) * 2900
-      );
-      expect(gasUsedEth).not.equal(0);
-    });
-  });
+  //     console.log(
+  //       "Total Gas USED: " +
+  //         ethers.utils.formatEther(gasUsedEth.toString()) * 2900
+  //     );
+  //     expect(gasUsedEth).not.equal(0);
+  //   });
+  // });
 });
